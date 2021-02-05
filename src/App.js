d69bb42e22c8b1d2fcf6b4a968beaa6bfd77b4ee
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-02-04 16:05:10 -0800

    adding resume file again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import HomeContent from './pages/home/home.js'
 import ProjectContent from './pages/projects/projects.js'
 import ContactContent from './pages/contact/contact.js';
-import MyResume from './pages/resumeFile/my_resume.pdf'
+import MyResume from './pages/resumeFile/Christian Parks Resume.pdf'
 
 const App = () => {
   return (
